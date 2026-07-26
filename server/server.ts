e1c1655ae8302d25032a3c3f16e89c3d2400e90e
khiry13 <ahmedkhiry414@gmail.com>
2026-07-26 15:13:10 +0300

    create api for authentication

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,8 @@
 import "dotenv/config";
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from "cors";
 import connectDB from "./config/db.js";
+import authRouter from "./routes/authRoutes.js";
 
 const app = express();
 
@@ -16,6 +17,17 @@ const port = process.env.PORT || 5000;
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Server is Live!');
+});
+
+app.use("/api/auth", authRouter);
+
+// Global error handler
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    console.error("Unhandle error: ", err);
+    res.status(500).json({
+        error: err.message || "Internal Server Error",
+        stack: process.env.NODE_ENV === "production" ? undefined : err.stack
+    })
 });
 
 app.listen(port, () => {
